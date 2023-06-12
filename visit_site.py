ef40abbd2ff5c71aef6570778da9cae5179d7141
pamelaolney <pjolney12@gmail.com>
2023-06-12 19:35:12 -0400

    Add sleep timer

--- visit_site.py
+++ visit_site.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
 
 
 options = Options()
@@ -16,4 +17,10 @@ driver.get("https://www.reddit.com")
 driver.maximize_window()
 login_link = driver.find_element(By.LINK_TEXT, "Log In")
 login_link.click()
-print(login_link)
+time.sleep(3)
+
+driver.close()
+
+# find_google = driver.find_elements(By.CSS_SELECTOR, 'span:contains("Continue with Google")').click()
+# find_google
+# print(find_google)
