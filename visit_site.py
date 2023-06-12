3df64e298af811a4c4ea310927c35ccdde934847
pamelaolney <pjolney12@gmail.com>
2023-06-11 23:24:05 -0400

    Disable notifications

--- visit_site.py
+++ visit_site.py
@@ -5,9 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_experimental_option("detach", True)
+options.add_argument("--disable-notifications") 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                           options=options)
                         
-driver.get("https://www.reddit.com") 
+driver.get("https://www.reddit.com")
+driver.maximize_window()
+# driver.find
 
